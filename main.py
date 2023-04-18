46f67a3083dd30ccfa45f6392b00471cda572ba9
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-18 18:40:57 +0200

    v0.1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 def send(number: str, message: str):
-    message_arg = f'"{message}"'
+    message_arg = f'"{message}"'.replace(" ", "\ ")
 
     # Call the adb shell service call command with the phone number and message text arguments
     os.system(f'adb shell service call isms 7 i32 1 s16 "com.android.mms.service" s16 "{number}" s16 "null" s16 {message_arg} s16 "null" s16 "null"')

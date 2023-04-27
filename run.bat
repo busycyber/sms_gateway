bcf8081befcee67f11705f1fbefbcdbc368eb290
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-27 15:27:06 +0200

    test

--- run.bat
+++ run.bat
@@ -1,18 +1,14 @@
 @echo off
 
-setlocal enableextensions enabledelayedexpansion
-
-set script_path=%~dp0
-set script_path=%script_path:~0,-1%
-
 :loop
-tasklist /nh /fi "imagename eq python.exe" | find /i "server.py" > nul
-if not errorlevel 1 (
-    for /f "tokens=2" %%a in ('tasklist /fi "imagename eq python.exe" /fi "windowtitle eq %script_path:~2%" /fo list ^| find "PID:"') do set "pid=%%a"
-    echo Server is running with PID %pid%, killing process...
-    taskkill /f /pid %pid% > nul
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :9000 ^| findstr LISTENING') do set PID=%%a
+
+if not "%PID%"=="" (
+    echo Server is already running with PID %PID%.
+    timeout /t 5 >nul
+) else (
+    echo Starting server...
+    start /b pythonw server.py >> log.txt 2>&1
 )
-echo Starting server...
-start /b "" pythonw %script_path%\server.py >> %script_path%\log.txt 2>&1
-timeout /t 5 > nul
+
 goto loop

17afd13c0cb48d9f28393f09a6529952e357a35a
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-27 22:18:43 +0200

    v1:

--- run.bat
+++ run.bat
@@ -1,17 +1,1 @@
-@echo off
-
-set port=9000
-
-:loop
-for /f "tokens=2" %%a in ('netstat -aon ^| findstr :%port% ^| findstr LISTENING') do set PID=%%a
-
-if not "%PID%"=="" (
-    echo Server is already running with PID %PID%. Killing process...
-    taskkill /F /PID %PID%
-    timeout /t 5 >nul
-) else (
-    echo Starting server...
-    start /b pythonw server.py >> log.txt 2>&1
-)
-
-goto loop
+python gui.py

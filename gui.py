c7d48722449b942887ec1d50fb687c062c49c041
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-04-28 13:40:54 +0200

    scraped server idea, just gui for now

--- gui.py
+++ gui.py
@@ -1,16 +1,22 @@
 import PySimpleGUI as sg
 from main import send
+import os
 
 sg.theme('BlueMono')
-layout = [[sg.Text('Recievers', font='Arial 32')],
-          [sg.Multiline(size=(400, 10), key='recievers', font='Arial 22')],
-          [sg.Text('Message', font='Arial 32')],
-          [sg.Multiline(size=(400, 10), key='message', font='Arial 22')],
+layout = [
+    [sg.Text('Recievers', font='Arial 32')],
+    [sg.Multiline(size=(400, 10), key='recievers', font='Arial 22')],
+    [sg.Text('Message', font='Arial 32')],
+    [sg.Multiline(size=(400, 10), key='message', font='Arial 22')],
 
-          [sg.Button('Send', font="Arial 24"), sg.Button('Close', font="Arial 24")]]
+    [sg.Button('Send', font="Arial 24"), sg.Button('Close', font="Arial 24")]]
 
 
 window = sg.Window('Test', layout, size=(800, 800)).Finalize()
+
+os.system("adb kill-server")
+os.system("adb tcpip 5555")
+os.sytem(f"adb connect {os.environ['PHONE_IP']}:5555")
 
 
 while True:
